7109e1c69af7f0765b4d5a247744a62debb8974e
SiMamank115 <faeest.115@gmail.com>
2023-04-24 12:27:16 +0700

    dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
-import { Box, Button, ButtonGroup, Container, FormLabel,IconButton,Tooltip, Image, Input, InputGroup, Spinner, useColorMode, Link, textDecoration, Menu, MenuButton, MenuList, MenuItem, } from "@chakra-ui/react";
+import { Box, Button, ButtonGroup, Container, FormLabel, IconButton, Tooltip, Image, Input, InputGroup, Spinner, useColorMode, Link, textDecoration, Menu, MenuButton, MenuList, MenuItem, useDisclosure, Fade, Collapse } from "@chakra-ui/react";
 import ModeToggler from "./ModeToggler.js";
 import { css } from "@emotion/react";
 import { listenAuth, redir } from "@/helpers/redirect.js";
@@ -12,26 +12,34 @@ import NavbarLink from "./Navbarlink.js";
 
 export default function Navbar() {
     const { colorMode } = useColorMode(),
-    supabase = useSupabaseClient();
-
+        { isOpen, onToggle } = useDisclosure(),
+        supabase = useSupabaseClient();
     return (
         <Box w={"full"} position={"sticky"} alignItems={"center"} justifyContent={"space-around"} display={"flex"} h={"70px"} shadow={"md"} bg={"palette.white"} _dark={{ bg: "palette.dark" }}>
             <Link h={"40%"} href="/">
-                <Image h={"100%"} src={colorMode == "dark" ? "/images/logo-white.png" : "/images/logo.png"} alt="logo-harmonyhires"/>
+                <Image h={"100%"} src={colorMode == "dark" ? "/images/logo-white.png" : "/images/logo.png"} alt="logo-harmonyhires" />
             </Link>
 
-            <ul href="/about-us"  css={css(`list-style:none;display:flex;gap:3rem;`)}>
-                <li><NavbarLink href="/">About us</NavbarLink></li>
-                <li className="drophover"><NavbarLink href="/">FAQ</NavbarLink>
-                <Box className="menu">
-                    <NavbarLink href="/">Test</NavbarLink>
-                </Box>
-
+            <ul href="/about-us" css={css(`list-style:none;display:flex;gap:3rem;`)}>
+                <li>
+                    <NavbarLink href="/">About us</NavbarLink>
                 </li>
-                <li><NavbarLink href="/">Job</NavbarLink></li>
-                <li><NavbarLink href="/">Talent Finder</NavbarLink></li>
+                <li className="drophover" onMouseEnter={()=>!isOpen && onToggle()} onMouseLeave={()=>isOpen && onToggle()}>
+                    <NavbarLink href="/">FAQ</NavbarLink>
+                    <Collapse in={isOpen} animateOpacity>
+                        <Box fontWeight={500} className="menu" borderRadius={"base"} shadow={"lg"} color={colorMode == "light"?"palette.light" : "palette.dark"} bg={colorMode == "light"?"palette.black" : "palette.light"}>
+                            <Link href="/" _hover={{textDecoration:"none"}}>Test</Link>
+                        </Box>
+                    </Collapse>
+                </li>
+                <li>
+                    <NavbarLink href="/">Job</NavbarLink>
+                </li>
+                <li>
+                    <NavbarLink href="/">Talent Finder</NavbarLink>
+                </li>
             </ul>
-            
+
             <ButtonGroup>
                 <Tooltip hasArrow placement="top" label="Theme" bg="palette.dark" color="palette.light">
                     <Box overflow={"hidden"}>
@@ -39,7 +47,16 @@ export default function Navbar() {
                     </Box>
                 </Tooltip>
                 <Tooltip hasArrow placement="top" label="Log Out" bg="palette.dark" color="palette.light">
-                    <IconButton borderRadius={"2sm"} colorScheme="mint" onClick={async () => {await supabase.auth.signOut();redir("/auth/login")}} variant="outline" icon={<FontAwesomeIcon icon={solid.faRightFromBracket} />} />
+                    <IconButton
+                        borderRadius={"2sm"}
+                        colorScheme="mint"
+                        onClick={async () => {
+                            await supabase.auth.signOut();
+                            redir("/auth/login");
+                        }}
+                        variant="outline"
+                        icon={<FontAwesomeIcon icon={solid.faRightFromBracket} />}
+                    />
                 </Tooltip>
             </ButtonGroup>
         </Box>
--- src/components/Navbarlink.js
+++ src/components/Navbarlink.js
@@ -1,11 +1,20 @@
 import { Link, useColorMode } from "@chakra-ui/react";
 
 export default function NavbarLink(props) {
-    const { colorMode, toggleColorMode } = useColorMode();
-    let modified = structuredClone(props);
-    modified["_hover"] ? (modified["_hover"]["textDecoration"] = "none") : (modified._hover = { textDecoration: "none" });
-    modified["fontWeight"] ?? (modified["fontWeight"] = 600);
-    modified["fontSize"] ?? (modified["fontSize"] = "1.1rem");
-    modified["_hover"]["color"] ?? (modified["_hover"]["color"] = colorMode == "light" ? "mint.500" : "palette.mint");
-    return <Link {...modified}>{modified.children}</Link>;
+    const { colorMode } = useColorMode(),
+    initialProps = {
+        _hover: {
+            textDecoration: "none",
+            color : colorMode == "light" ? "mint.500" : "palette.mint"
+        },
+        fontWeight : 500,
+        fontSize : "1.1rem",
+        className:"navlink"
+    }
+    // modified["_hover"] ? (modified["_hover"]["textDecoration"] = "none") : (modified._hover = { textDecoration: "none" });
+    // modified["fontWeight"] ?? (modified["fontWeight"] = 500);
+    // modified["fontSize"] ?? (modified["fontSize"] = "1.1rem");
+    // modified["_hover"]["color"] ?? (modified["_hover"]["color"] = colorMode == "light" ? "mint.500" : "palette.mint");
+
+    return <Link {...initialProps} {...props}>{props.children}</Link>;
 }
